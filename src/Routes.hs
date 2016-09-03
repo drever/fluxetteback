0eee7aca6ae98250c639a4c85def2cb34d6d2470
Johannes Drever <drever@lrz.uni-muenchen.de>
2016-09-03 17:15:13 +0200

    New game

--- src/Routes.hs
+++ src/Routes.hs
@@ -15,17 +15,39 @@ type FluxetteAPI = "js" :> Raw
                :<|> "css" :> Raw
                :<|> "html" :> Raw
                :<|> GetCurrentGame
+               :<|> SetCurrentGame
+               :<|> NewGame
 
 type GetCurrentGame = "currentGame" :> Get '[JSON] Game
+type SetCurrentGame = "setCurrentGame" :> ReqBody '[JSON] Game :> Post '[JSON] Game
+type NewGame = "newGame" :> Post '[JSON] Game
 
 server v = serveDirectory "../fluxette/js-build/install-root/bin/fluxette.jsexe"
     :<|> serveDirectory "webroot/css"
     :<|> serveDirectory "webroot/html"
     :<|> currentGame v
+    :<|> setCurrentGame v
+    :<|> newGame v
 
 currentGame :: MVar Game -> EitherT ServantErr IO Game
 currentGame v = do
   x <- liftIO $ readMVar v
   return x
 
+setCurrentGame :: MVar Game -> Game -> EitherT ServantErr IO Game
+setCurrentGame v g = do
+  liftIO $ putStrLn "Set current game"
+  liftIO $ putStrLn $ (show g)
+  x <- liftIO $ takeMVar v
+  liftIO $ putMVar v g
+  return g
+
+newGame :: MVar Game -> EitherT ServantErr IO Game
+newGame v = do
+  liftIO $ do
+    putStrLn "New game"
+    x <- takeMVar v
+    g <- initGame
+    putMVar v g
+    return g
 
